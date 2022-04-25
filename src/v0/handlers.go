fbd5f234a238c6726855fbe597370076402c4e2e
Evonicgu <evonicgu@gmail.com>
2022-04-25 23:24:50 +0300

    Added refresh token handling

--- src/v0/handlers.go
+++ src/v0/handlers.go
@@ -5,10 +5,12 @@ import (
 	"errors"
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v4"
 	"google.golang.org/api/idtoken"
 	"gorm.io/gorm"
 	"os"
 	"presentio-server-user/src/v0/models"
+	"strconv"
 )
 
 type Config struct {
@@ -20,6 +22,8 @@ var apiKey = os.Getenv("GOOGLE_API_CLIENT_ID")
 func SetupRouter(group *gin.RouterGroup, config *Config) {
 	group.POST("/register", registerHandler(config))
 	group.POST("/authorize", authorizeHandler(config))
+	group.GET("/info/:id", userInfoHandler(config))
+	group.GET("/refresh", refreshHandler(config))
 }
 
 type UserParams struct {
@@ -78,7 +82,14 @@ func registerHandler(config *Config) func(*gin.Context) {
 			return
 		}
 
-		token, err := createNewToken(user.ID)
+		accessToken, err := createNewAccessToken(user.ID)
+
+		if err != nil {
+			c.Status(500)
+			return
+		}
+
+		refreshToken, err := createNewRefreshToken(user.ID)
 
 		if err != nil {
 			c.Status(500)
@@ -86,7 +97,8 @@ func registerHandler(config *Config) func(*gin.Context) {
 		}
 
 		c.JSON(200, gin.H{
-			"token": token,
+			"accessToken":   accessToken,
+			"refresh_token": refreshToken,
 		})
 	}
 }
@@ -123,9 +135,18 @@ func authorizeHandler(config *Config) func(*gin.Context) {
 			} else {
 				c.Status(500)
 			}
+
+			return
 		}
 
-		token, err := createNewToken(user.ID)
+		accessToken, err := createNewAccessToken(user.ID)
+
+		if err != nil {
+			c.Status(500)
+			return
+		}
+
+		refreshToken, err := createNewRefreshToken(user.ID)
 
 		if err != nil {
 			c.Status(500)
@@ -133,7 +154,101 @@ func authorizeHandler(config *Config) func(*gin.Context) {
 		}
 
 		c.JSON(200, gin.H{
-			"token": token,
+			"accessToken":   accessToken,
+			"refresh_token": refreshToken,
+		})
+	}
+}
+
+func userInfoHandler(config *Config) func(*gin.Context) {
+	return func(c *gin.Context) {
+		authHeader := c.GetHeader("Authorization")
+
+		token, err := validateAccessTokenHeader(authHeader)
+
+		if err != nil {
+			if errors.Is(err, jwt.ErrTokenMalformed) {
+				c.Status(406)
+			} else if errors.Is(err, jwt.ErrTokenExpired) {
+				c.Status(408)
+			} else {
+				c.Status(400)
+			}
+
+			return
+		}
+
+		claims, ok := token.Claims.(*UserClaims)
+
+		if !ok {
+			c.Status(403)
+			return
+		}
+
+		userId, err := strconv.ParseInt(c.Param("id"), 10, 64)
+
+		if err != nil {
+			c.Status(404)
+			return
+		}
+
+		var user models.User
+
+		db := config.Db
+
+		result := db.Where("id = ?", userId).First(&user)
+
+		if result.Error != nil {
+			if errors.Is(result.Error, gorm.ErrRecordNotFound) {
+				c.Status(404)
+			} else {
+				c.Status(500)
+			}
+
+			return
+		}
+
+		c.JSON(200, gin.H{
+			"self": user.ID == claims.ID,
+			"user": user,
+		})
+	}
+}
+
+func refreshHandler(config *Config) func(*gin.Context) {
+	return func(c *gin.Context) {
+		authHeader := c.GetHeader("Authorization")
+
+		token, err := validateRefreshTokenHeader(authHeader)
+
+		if err != nil {
+			if errors.Is(err, jwt.ErrTokenMalformed) {
+				c.Status(406)
+			} else if errors.Is(err, jwt.ErrTokenExpired) {
+				c.Status(408)
+			} else {
+				c.Status(400)
+			}
+
+			return
+		}
+
+		claims, ok := token.Claims.(*UserClaims)
+
+		if !ok {
+			c.Status(403)
+			return
+		}
+
+		accessToken, err := createNewAccessToken(claims.ID)
+
+		if err != nil {
+			c.Status(500)
+			return
+		}
+
+		c.JSON(200, gin.H{
+			"token": accessToken,
 		})
 	}
 }
--- src/v0/token.go
+++ src/v0/token.go
@@ -4,6 +4,7 @@ import (
 	"crypto/rsa"
 	"github.com/golang-jwt/jwt/v4"
 	"os"
+	"strings"
 	"time"
 )
 
@@ -13,29 +14,88 @@ type UserClaims struct {
 	ID int64
 }
 
-var signKey *rsa.PrivateKey
+var accessSignKey *rsa.PrivateKey
+var accessValidateKey *rsa.PublicKey
+
+var refreshSignKey *rsa.PrivateKey
+var refreshValidateKey *rsa.PublicKey
 
 func init() {
 	var err error
 
-	signKey, err = jwt.ParseRSAPrivateKeyFromPEM([]byte(os.Getenv("TOKEN_PRIVATE_KEY")))
+	accessSignKey, err = jwt.ParseRSAPrivateKeyFromPEM([]byte(os.Getenv("TOKEN_PRIVATE_KEY")))
 
 	if err != nil {
 		panic("Unable to read RSA private key")
 	}
+
+	accessValidateKey, err = jwt.ParseRSAPublicKeyFromPEM([]byte(os.Getenv("TOKEN_PUBLIC_KEY")))
+
+	if err != nil {
+		panic("Unable to read RSA public key")
+	}
 }
 
-func createNewToken(id int64) (string, error) {
+func createNewAccessToken(id int64) (string, error) {
+	return createNewToken(id, &jwt.NumericDate{
+		Time: time.Now().Add(time.Minute * 5),
+	}, accessSignKey)
+}
+
+func createNewRefreshToken(id int64) (string, error) {
+	return createNewToken(id, &jwt.NumericDate{
+		Time: time.Now().AddDate(0, 1, 0),
+	}, refreshSignKey)
+}
+
+func createNewToken(id int64, expires *jwt.NumericDate, key *rsa.PrivateKey) (string, error) {
 	token := jwt.New(jwt.SigningMethodRS256)
 
 	token.Claims = &UserClaims{
 		RegisteredClaims: &jwt.RegisteredClaims{
-			ExpiresAt: &jwt.NumericDate{
-				Time: time.Now().Add(time.Minute * 5),
+			ExpiresAt: expires,
+			IssuedAt: &jwt.NumericDate{
+				Time: time.Now(),
 			},
 		},
 		ID: id,
 	}
 
-	return token.SignedString(signKey)
+	return token.SignedString(key)
+}
+
+func validateAccessTokenHeader(authHeader string) (*jwt.Token, error) {
+	return validateTokenHeader(authHeader, accessValidateKey)
+}
+
+func validateAccessToken(tokenStr string) (*jwt.Token, error) {
+	return validateToken(tokenStr, accessValidateKey)
+}
+
+func validateRefreshTokenHeader(authHeader string) (*jwt.Token, error) {
+	return validateTokenHeader(authHeader, refreshValidateKey)
+}
+
+func validateRefreshToken(tokenStr string) (*jwt.Token, error) {
+	return validateToken(tokenStr, refreshValidateKey)
+}
+
+func validateTokenHeader(authHeader string, key *rsa.PublicKey) (*jwt.Token, error) {
+	if authHeader == "" {
+		return nil, jwt.ErrTokenMalformed
+	}
+
+	parts := strings.Split(authHeader, " ")
+
+	if len(parts) != 2 || parts[1] != "Bearer" {
+		return nil, jwt.ErrTokenMalformed
+	}
+
+	return validateToken(parts[1], key)
+}
+
+func validateToken(tokenStr string, key *rsa.PublicKey) (*jwt.Token, error) {
+	return jwt.ParseWithClaims(tokenStr, UserClaims{}, func(token *jwt.Token) (interface{}, error) {
+		return key, nil
+	}, jwt.WithValidMethods([]string{"RS256"}))
 }

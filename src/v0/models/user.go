3cc522ebbf8ca9f1f60a88fe3562a609a9d88a6e
Evonicgu <evonicgu@gmail.com>
2022-04-30 12:13:45 +0300

    Updated json binding names

--- src/v0/models/user.go
+++ src/v0/models/user.go
@@ -2,9 +2,9 @@ package models
 
 type User struct {
 	ID        int64  `gorm:"primaryKey" json:"id"`
-	FirstName string `json:"first_name" binding:"required"`
-	LastName  string `json:"last_name" binding:"required"`
+	FirstName string `json:"firstName" binding:"required"`
+	LastName  string `json:"lastName" binding:"required"`
 	Email     string `json:"email" binding:"required"`
-	PFPUrl    string `json:"pfp_url" binding:"required"`
+	PFPUrl    string `json:"pfpUrl" binding:"required"`
 	Bio       string `json:"bio" binding:"required"`
 }

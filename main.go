657526dca748c5d85316dd93d1e23691ae9c0c41
Evonicgu <evonicgu@gmail.com>
2022-05-12 00:34:26 +0300

    Removed unnecessary logger creation

--- main.go
+++ main.go
@@ -42,8 +42,6 @@ func main() {
 		log.Fatalf("Unable to connect to postgres database at %s\n", dbUrl)
 	}
 
-	logger := log.Logger{}
-
 	router := gin.Default()
 
 	v0.SetupRouter(router.Group("/v0"), &v0.Config{Db: db})
@@ -51,6 +49,6 @@ func main() {
 	err = router.Run()
 
 	if err != nil {
-		logger.Fatalln("Failed to start server on port %s", os.Getenv("PORT"))
+		log.Fatalf("Failed to start server on port %s\n", os.Getenv("PORT"))
 	}
 }
